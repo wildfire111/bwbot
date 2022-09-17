e2c73e50ed99da468dc57f2a897ad5a6e0c5f101
wildfire111 <106965615+wildfire111@users.noreply.github.com>
2022-09-17 20:35:26 +1000

    commit

--- bwbot.py
+++ bwbot.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 load_dotenv()
 apikey = os.getenv('ALCH_KEY')
 url = 'https://arb-mainnet.g.alchemy.com/v2/'+apikey
+targetaddress = os.getenv('OWNER')
 
-blockhex = Web3.toHex(24421014)
 increasepostopic = '0x2fe68525253654c21998f35787a8d0f361905ef647c854092430ab65f2f15022'
 decreasepostopic = '0x93d75d64d1f84fc6f430a64fc578bdd4c1e090e90ea2d51773e626d19de56d30'
 tokenlist = {
@@ -16,89 +16,108 @@ tokenlist = {
     'usdc':'0xff970a61a04b1ca14834a43f5de4533ebddb5cc8',
     'weth':'0x82af49447d8a07e3bd95bd0d56f35241523fbab1',
 }
-print(tokenlist)
-payload = {
-    "id": 1,
-    "jsonrpc": "2.0",
-    "method": "eth_getLogs",
-    "params": [
-        {
-            "fromBlock": Web3.toHex(24421028),
-            "toBlock": Web3.toHex(24421028),
-            #"fromAddress": "0x3D6bA331e3D9702C5e8A8d254e5d8a285F223aba",
-            "Address":"0x489ee077994B6658eAfA855C308275EAd8097C4A",
-            #"blockHash":"0x0a063d41a043c0395f3b29dbf5eaf74f08a2eb609bd88ea0530d97dc18f3c8b5"
-            #"contractAddresses": ["0x3D6bA331e3D9702C5e8A8d254e5d8a285F223aba"],
-            #"category": ["external"],
-            #"order": "asc",
-            #"withMetadata": False,
-            #"excludeZeroValue": True,
-            #"maxCount": "0x3e8",
-            #"pageKey": "string"
-        }
 
-    ]
-}
-headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json"
-}
+startblock = 24421028
+
+#getting current block
+payload = {"jsonrpc": "2.0", "id": 0, "method": "eth_blockNumber"}
+headers = {"Accept": "application/json","Content-Type": "application/json"}
 response = requests.post(url, json=payload, headers=headers)
-if response.ok == True:
-    print("TX OK")
-else:
-    print("OH NO")
-
 respdict = response.json()
-datatypes = ['Key','AccAddress','Collateral','Index','CollatDelta','SizeDelta','IsLong','Price','Fee']
+curblock = Web3.toInt(hexstr=respdict['result'])
+print(f"Current block: {curblock}")
+blockstoprocess = curblock - startblock
+progress = 0
+while True:
+    print(str(round(progress/blockstoprocess*100))+'%')
+    progress = progress + 1000
+    if startblock >= curblock:
+        break
+    targetblock = startblock + 1000
+    if targetblock >= curblock:
+        targetblock = curblock
+    payload = {
+        "id": 1,
+        "jsonrpc": "2.0",
+        "method": "eth_getLogs",
+        "params": [
+            {
+                "fromBlock": Web3.toHex(startblock),
+                "toBlock": Web3.toHex(targetblock),
+                #"fromAddress": "0x3D6bA331e3D9702C5e8A8d254e5d8a285F223aba",
+                "Address":"0x489ee077994B6658eAfA855C308275EAd8097C4A",
+                #"blockHash":"0x0a063d41a043c0395f3b29dbf5eaf74f08a2eb609bd88ea0530d97dc18f3c8b5"
+                #"contractAddresses": ["0x3D6bA331e3D9702C5e8A8d254e5d8a285F223aba"],
+                #"category": ["external"],
+                #"order": "asc",
+                #"withMetadata": False,
+                #"excludeZeroValue": True,
+                #"maxCount": "0x3e8",
+                #"pageKey": "string"
+            }
 
-#print(dict['result'][0]['address'])
-for tx in respdict['result']:
-#    print(Web3.toInt(hexstr=tx['blockNumber']))
-    for txtopic in tx['topics']:
-        if tx['topics'][0] != increasepostopic and tx['topics'][0] != decreasepostopic:
-            break
-        print(tx['transactionHash'])
-        if tx['topics'][0] == increasepostopic:
-            print('Position increasing')
-        else:
-            print('Position decreasing')
-        txdata = tx['data']
-        txdata = txdata[2:]
-        txdatalist = list()
-        if len(txdata)%64 != 0:
-            print('Error in data')
-            break
-        parseddata = dict()
-        for i in range(int(len(txdata)/64)):
-            txdatalist.append(txdata[0:64])
-            if len(txdata) != 0:
-                txdata = txdata[64:]
-        for i in range(len(txdatalist)):
-            parseddata[datatypes[i]] = txdatalist[i]
-        #print(parseddata)
-        #Web3.toInt(hexstr=parseddata['CollatDelta'])
-        parseddata['Collateral'] = (Web3.toHex(hexstr=parseddata['Collateral'][24:]))
-        parseddata['Index'] = (Web3.toHex(hexstr=parseddata['Index'][24:]))
-        parseddata['AccAddress'] = (Web3.toHex(hexstr=parseddata['AccAddress'][24:]))
-        #print(Web3.toInt(hexstr=parseddata['Price']))
-        parseddata['Price'] = Web3.toInt(hexstr=parseddata['Price'])/(10**30)
-        parseddata['CollatDelta'] = Web3.toInt(hexstr=parseddata['CollatDelta'])/(10**30)
-        parseddata['SizeDelta'] = Web3.toInt(hexstr=parseddata['SizeDelta'])/(10**30)
-        parseddata['Fee'] = Web3.toInt(hexstr=parseddata['Fee'])/(10**30)
-        for a,b in tokenlist.items():
-            if parseddata['Collateral'] == b:
-                parseddata['Collateral'] = a
-            if parseddata['Index'] == b:
-                parseddata['Index'] = a
-        if parseddata['Collateral'] not in tokenlist or parseddata['Index'] not in tokenlist:
-            print("Couldn't find a token")
-        if int(parseddata['IsLong']) == 1:
-            parseddata['IsLong'] = True
-        else:
-            parseddata['IsLong'] = False
-        for key,value in parseddata.items():
-            print(f"{key} - {value}")
+        ]
+    }
+    startblock = targetblock + 1
+    response = requests.post(url, json=payload, headers=headers)
+    if response.ok == True:
+        print("TX OK")
+    else:
+        print("OH NO")
+
+    respdict = response.json()
+    datatypes = ['Key','AccAddress','Collateral','Index','CollatDelta','SizeDelta','IsLong','Price','Fee']
+
+    #print(dict['result'][0]['address'])
+    for tx in respdict['result']:
+    #    print(Web3.toInt(hexstr=tx['blockNumber']))
+        for txtopic in tx['topics']:
+            if tx['topics'][0] != increasepostopic and tx['topics'][0] != decreasepostopic:
+                break
+            #print(tx['transactionHash'])
+            #if tx['topics'][0] == increasepostopic:
+            #    print('Position increasing')
+            #else:
+            #    print('Position decreasing')
+            txdata = tx['data']
+            txdata = txdata[2:]
+            txdatalist = list()
+            if len(txdata)%64 != 0:
+                print('Error in data')
+                break
+            parseddata = dict()
+            for i in range(int(len(txdata)/64)):
+                txdatalist.append(txdata[0:64])
+                if len(txdata) != 0:
+                    txdata = txdata[64:]
+            for i in range(len(txdatalist)):
+                parseddata[datatypes[i]] = txdatalist[i]
+            parseddata['AccAddress'] = (Web3.toHex(hexstr=parseddata['AccAddress'][24:]))
+            if parseddata['AccAddress'].upper() != str(targetaddress).upper():
+            #    print('No account match')
+                break
+            Web3.toInt(hexstr=parseddata['CollatDelta'])
+            parseddata['Collateral'] = (Web3.toHex(hexstr=parseddata['Collateral'][24:]))
+            parseddata['Index'] = (Web3.toHex(hexstr=parseddata['Index'][24:]))
+            #print(Web3.toInt(hexstr=parseddata['Price']))
+            parseddata['Price'] = Web3.toInt(hexstr=parseddata['Price'])/(10**30)
+            parseddata['CollatDelta'] = Web3.toInt(hexstr=parseddata['CollatDelta'])/(10**30)
+            parseddata['SizeDelta'] = Web3.toInt(hexstr=parseddata['SizeDelta'])/(10**30)
+            parseddata['Fee'] = Web3.toInt(hexstr=parseddata['Fee'])/(10**30)
+            for a,b in tokenlist.items():
+                if parseddata['Collateral'] == b:
+                    parseddata['Collateral'] = a
+                if parseddata['Index'] == b:
+                    parseddata['Index'] = a
+            if parseddata['Collateral'] not in tokenlist or parseddata['Index'] not in tokenlist:
+                print("Couldn't find a token")
+            if int(parseddata['IsLong']) == 1:
+                parseddata['IsLong'] = True
+            else:
+                parseddata['IsLong'] = False
+            for key,value in parseddata.items():
+                print(f"{key} - {value}")
+            print('Block: '+str(Web3.toInt(hexstr=tx['blockNumber'])))
 
 
 
