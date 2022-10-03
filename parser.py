e257596d7a9eabc5ff26b4a3a6c00035a94cff2a
wildfire111 <106965615+wildfire111@users.noreply.github.com>
2022-10-03 20:32:25 +1100

    Update parser.py

--- parser.py
+++ parser.py
@@ -6,3 +6,13 @@ import os
 load_dotenv()
 
 owner = os.getenv('OWNER')
+
+con = sqlite3.connect('transactions.db')
+cur = con.cursor()
+
+cur.execute(f'SELECT * FROM transactions WHERE AccAddress = "{owner}"')
+transactions = cur.fetchall()
+for tx in transactions:
+    print(tx)
+
+
